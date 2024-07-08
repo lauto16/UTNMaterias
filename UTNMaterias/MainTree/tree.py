28f0c07a9f25e34fdd1f70f1ef5f01aaf3508053
lauto16 <ltdev658@gmail.com>
2024-07-08 17:05:15 -0300

    Tree and Node

---
+++ UTNMaterias/MainTree/tree.py
@@ -0,0 +1,72 @@
+class Subject():
+    def __init__(self, sql_id, name, is_approved, is_regular, is_enrollable) -> None:
+        self.sql_id = sql_id
+        self.name = name
+        self.is_approved = is_approved
+        self.is_regular = is_regular
+        self.is_enrollable = is_enrollable
+        self.children = {
+            'approved': [],
+            'regular': []
+        }
+
+    def __str__(self) -> str:
+        r = str(
+            f'ID: {self.sql_id}\n' +
+            f'NAME: {self.name}\n' +
+            f'REGULAR_CHILDREN: {self.children["regular"]}\n' +
+            f'APPROVED_CHILDREN: {self.children["approved"]}\n' +
+            f'IS_APPROVED: {self.is_approved}\n' +
+            f'IS_REGULAR: {self.is_regular}\n' +
+            f'IS_ENROLLABLE: {self.is_enrollable}\n'
+        )
+        return r
+
+    def newChild(self, subject, subject_conection) -> None:
+        if subject_conection == 'approved':
+            self.children['approved'].append(subject)
+        elif subject_conection == 'regular':
+            self.children['regular'].append(subject)
+
+
+class Tree():
+    def __init__(self) -> None:
+        self.root = Subject(sql_id=1, name='Ingreso',
+                            is_approved=False, is_regular=False, is_enrollable=True)
+
+    def search(self, sql_id, subject_connection: str):
+        if subject_connection == 'approved':
+            return self._recursiveApprovedSearch(sql_id, self.root)
+        elif subject_connection == 'regular':
+            return self._recursiveRegularSearch(sql_id, self.root)
+
+    def _recursiveApprovedSearch(self, sql_id: int, actualSubject: Subject):
+        if actualSubject.sql_id == sql_id:
+            return actualSubject
+
+        for child in actualSubject.children['approved']:
+            found = self._recursiveApprovedSearch(sql_id, child)
+            if found:
+                return found
+
+        return None
+
+    def _recursiveRegularSearch(self, sql_id: int, actualSubject: Subject):
+        if actualSubject.sql_id == sql_id:
+            return actualSubject
+
+        for child in actualSubject.children['regular']:
+            found = self._recursiveRegularSearch(sql_id, child)
+            if found:
+                return found
+
+    def add(self, father_sql_id: int, child_subject: Subject, subject_conection: str):
+        father_subject_approved = self.search(father_sql_id, 'approved')
+        father_subject_regular = self.search(father_sql_id, 'regular')
+
+        if father_subject_approved:
+            father_subject_approved.newChild(child_subject, subject_conection)
+        elif father_subject_regular:
+            father_subject_approved.newChild(child_subject, subject_conection)
+        else:
+            print('Father doesnt exist')
