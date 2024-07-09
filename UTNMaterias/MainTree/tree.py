a37a45560305ff1129b130d106b62bb32c4523dd
lauto16 <ltdev658@gmail.com>
2024-07-09 18:18:41 -0300

    tree.py commented

--- UTNMaterias/MainTree/tree.py
+++ UTNMaterias/MainTree/tree.py
@@ -1,5 +1,27 @@
 class Subject():
-    def __init__(self, sql_id, name, is_approved, is_regular, is_enrollable) -> None:
+    """
+    This class represents a Node of the Subjects tree.
+    Note: At UTN FRC (Universidad Tecnologica Nacional, Facultad Regional Cordoba), there's plenty states that a student can obtain
+    by studying any subject, but we only need 2 of them to build this app -> Regular and Approved
+
+    Regular: The student has passed all of his exams but he still need's to pass the final exam.
+    Approved: The student has passed all of his exams, including the final exam.
+
+    Each subject established when a student is capable of registering, it can be either by being "regular" or "approved" on any
+    other previous subject
+
+    Attributes:
+        self.sql_id: int -> Represents the sql database id 
+        self.name: str -> The subject's name
+        self.is_approved: bool -> True if the user completely finished the subject
+        self.is_regular: bool -> True if the user is regular at the subject
+        self.is_enrollable: bool -> True if the user can register for the subject 
+        self.children = {'approved': [],'regular': []}: dict{list} -> Contains two lists: 
+            (approved) represents the children(Subject) whose connection to the father(current Subject) is the need to be approved
+            (regular) represents the children(Subject) whose connection to the father(current Subject) is the need to be regular 
+    """
+
+    def __init__(self, sql_id: int, name: str, is_approved: bool, is_regular: bool, is_enrollable: bool) -> None:
         self.sql_id = sql_id
         self.name = name
         self.is_approved = is_approved
@@ -22,7 +44,15 @@ class Subject():
         )
         return r
 
-    def newChild(self, subject, subject_conection) -> None:
+    def newChild(self, subject, subject_conection: str) -> None:
+        """
+        Adds a new Subject child to the current Subject 
+
+        Args:
+            subject (Subject): Subject object
+            subject_conection (str): 'approved' or 'regular'
+        """
+
         if subject_conection == 'approved':
             self.children['approved'].append(subject)
         elif subject_conection == 'regular':
@@ -30,17 +60,47 @@ class Subject():
 
 
 class Tree():
+    """
+    A Tree that containt Subject objects as nodes and 'approved' or 'regular' as connectors
+
+    Attributes:
+        self.root: Subject -> Represents the 'Ingreso' Subject, which is the father of all nodes.
+    """
+
     def __init__(self) -> None:
         self.root = Subject(sql_id=1, name='Ingreso',
                             is_approved=False, is_regular=False, is_enrollable=True)
 
     def search(self, sql_id, subject_connection: str):
+        """
+        Searches a subject on the Tree by sql_id
+
+        Args:
+            sql_id (int): Represents the sql database id 
+            subject_connection (str): 'approved' or 'regular', for more info read the class documentation
+
+        Returns:
+            Subject: if the subject has been found
+            None: if the subject doesn't exist
+        """
         if subject_connection == 'approved':
             return self._recursiveApprovedSearch(sql_id, self.root)
         elif subject_connection == 'regular':
             return self._recursiveRegularSearch(sql_id, self.root)
 
     def _recursiveApprovedSearch(self, sql_id: int, actualSubject: Subject):
+        """
+        Recursively searches for a subject whose connection to the current subject (father) is 'approved'.
+        Starting from a given father Subject
+
+        Args:
+            sql_id (int): Represents the sql database id 
+            actualSubject (Subject): The subject object where the search is to start
+
+        Returns:
+            Subject: if the subject has been found
+            None: if the subject doesn't exist as a children
+        """
         if actualSubject.sql_id == sql_id:
             return actualSubject
 
@@ -52,6 +112,18 @@ class Tree():
         return None
 
     def _recursiveRegularSearch(self, sql_id: int, actualSubject: Subject):
+        """
+        Recursively searches for a subject whose connection to the current subject (father) is 'regular'.
+        Starting from a given father Subject
+
+        Args:
+            sql_id (int): Represents the sql database id 
+            actualSubject (Subject): The subject object where the search is to start
+
+        Returns:
+            Subject: if the subject has been found
+            None: if the subject doesn't exist as a children
+        """
         if actualSubject.sql_id == sql_id:
             return actualSubject
 
@@ -60,7 +132,15 @@ class Tree():
             if found:
                 return found
 
-    def add(self, father_sql_id: int, child_subject: Subject, subject_conection: str):
+    def add(self, father_sql_id: int, child_subject: Subject, subject_conection: str) -> None:
+        """
+        Adds a new child subject to a given father subject
+
+        Args:
+            father_sql_id (int): Father's sql id
+            child_subject (Subject): Child to add
+            subject_conection (str): Connection between father and child, must be 'approved' or 'regular'
+        """
         father_subject_approved = self.search(father_sql_id, 'approved')
         father_subject_regular = self.search(father_sql_id, 'regular')
 
