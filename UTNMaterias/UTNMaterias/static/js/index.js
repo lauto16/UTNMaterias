69e176903323a9cbed62aab1de5438c9380c71ca
lauto16 <ltdev658@gmail.com>
2024-07-17 15:43:00 -0300

    Funcion para consultar datos de la api desde el frontends

--- UTNMaterias/UTNMaterias/static/js/index.js
+++ UTNMaterias/UTNMaterias/static/js/index.js
@@ -5,7 +5,6 @@ function getCookie(name) {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
@@ -16,44 +15,41 @@ function getCookie(name) {
 }
 
 
-/* REEMPLAZAR CON CONSULTA CON API
 function getSubject(subject_id) {
+    /*
+    Returns the data of a Subject when subject_id is passed as an argument
+    (The data comes from SubjectAPI)
+
+    Usage:
+    
+    getSubject(13)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error fetching subject:', error);
+    });
+
+    */
+    const query = './apis/subjects/' + subject_id.toString();
 
     let requestOptions = {
-
-        method: 'POST',
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             "X-CSRFToken": getCookie('csrftoken'),
         },
-        // send 
-        body: JSON.stringify({
-            action: 'getSubject',
-            subject_id: subject_id
-        })
     };
 
-    fetch('./', requestOptions)
-
+    return fetch(query, requestOptions)
         .then(response => {
             if (response.ok) {
                 return response.json();
             }
             throw new Error('Network response was not ok.');
         })
-
-        .then(data => {
-            if (data.success === true)
-                console.log(data)
-            else{
-                //errorHandler(error=data.error)
-                console.log('error')
-            }
-            })
-
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
+            throw error; 
         });
-
 }
-*/
