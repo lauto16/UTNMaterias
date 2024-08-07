c29f5d433a41b78562005a7f9ada8016e03dec1a
lauto16 <ltdev658@gmail.com>
2024-08-07 19:09:50 -0300

    script 'correlativas.py' para automatizar el rellenado de la base de datos

--- UTNMaterias/MainTree/tests.py
+++ UTNMaterias/MainTree/tests.py
@@ -1,20 +1,21 @@
-"""from .models import UTNSubject
+from .models import *
 from .subject_tree import SubjectTreeDB
+from SubjectAPI.api import SubjectViewSet
 
 
-def from_fathers_to_children():
-    
-    #Using the fathers array from every db subject, sets the children of the fathers
-    
-    subjects = list(UTNSubject.objects.all())
+def from_fathers_to_children(career: str):
+
+    # Using the fathers array from every db subject, sets the children of the fathers
+    model = SubjectViewSet.get_model_for_career(career)
+    subjects = list(model.objects.all())
     for subject in subjects:
-        if subject.regular_fathers == 'sistemas':
+        if subject.regular_fathers == career:
             continue
 
         fathers_array = SubjectTreeDB.parseStrList(subject.regular_fathers)
         for father in fathers_array:
             try:
-                father_sql = UTNSubject.objects.get(id=father)
+                father_sql = model.objects.get(id=father)
                 if father_sql.regular_children == '':
                     father_sql.regular_children = str(subject.id)
                 else:
@@ -25,4 +26,3 @@ def from_fathers_to_children():
             except Exception as e:
                 print(e)
                 continue
-"""
--- UTNMaterias/MainTree/views.py
+++ UTNMaterias/MainTree/views.py
@@ -1,6 +1,55 @@
 from django.shortcuts import render
 from MainTree.subject_tree import *
 from django.http import Http404
+import json
+
+from .models import *
+from .subject_tree import SubjectTreeDB
+from SubjectAPI.api import SubjectViewSet
+
+
+def from_fathers_to_children(career: str):
+
+    # Using the fathers array from every db subject, sets the children of the fathers
+    model = SubjectViewSet.get_model_for_career(career)
+    subjects = list(model.objects.all())
+    for subject in subjects:
+        if subject.regular_fathers == career:
+            continue
+
+        fathers_array = SubjectTreeDB.parse_str_list(subject.regular_fathers)
+        for father in fathers_array:
+            try:
+                father_sql = model.objects.get(id=father)
+                if father_sql.regular_children == '':
+                    father_sql.regular_children = str(subject.id)
+                else:
+                    father_sql.regular_children = father_sql.regular_children + \
+                        str(f',{str(subject.id)}')
+
+                father_sql.save()
+            except Exception as e:
+                print(e)
+                continue
+
+    for subject in subjects:
+        if subject.approval_fathers == career:
+            continue
+
+        fathers_array = SubjectTreeDB.parse_str_list(subject.approval_fathers)
+        for father in fathers_array:
+            try:
+                father_sql = model.objects.get(id=father)
+                if father_sql.approval_children == '':
+                    father_sql.approval_children = str(subject.id)
+                else:
+                    father_sql.approval_children = father_sql.approval_children + \
+                        str(f',{str(subject.id)}')
+
+                father_sql.save()
+            except Exception as e:
+                print(e)
+                continue
 
 
 def index(request, career: str):
@@ -21,5 +70,7 @@ def index(request, career: str):
     if request.method == 'POST':
         data = json.loads(request.body)
         action = data['action']
+
+    # from_fathers_to_children(career)
 
     return render(request, 'index.html', {'career': career})
